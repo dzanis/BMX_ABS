975f1a9fb5810a330464d1345a31db53958c60a8
dzanis <zanisdukalskis@gmail.com>
2022-05-27 22:01:01 +0300

    point 2 changed and removed H-Bridge
    В пункте 2 убрана задержка 1000мс,чтобы насос сразу отключался если
    колесо разблокировалось.Потому что тестами выяснилось что хватает
    коротких импульсов включения.
    Пока что на время тестирования ABS поставлена меньшая (1000мс) задержка
    отключения насоса,если колесо не разблокировалось.
    H-Bridge (который подключался к соленойду клапана) был убран из схемы
    электронного блока управления из-за технических трудностей.
    Теперь соленойд управляется одним пином.

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <Ticker.h>
 
 #define PIN_HALL_EFFECT_SENSOR 5 // пин подключенный к выходу датчика холла
-#define PIN_SOLENOID_1 12 // пин подключенный к соленойду
-#define PIN_SOLENOID_2 14 // пин подключенный к соленойду
+#define PIN_VALVE 12 // пин подключенный к клапану
+
 #define PIN_PUMP 4 // пин подключенный к насосу
 
 #define IMPULSES_PER_SEC 25 // импульсов в секунду (10км/ч)
@@ -12,8 +12,7 @@
 #define MAX_IMPULSES_PER_CHECK_INTERVAL (CHECK_INTERVAL_MS/(1000/IMPULSES_PER_SEC))
 
 // через какое время отключить клапан и насос ( значения в миллисекундах)
-#define ALL_OFF_AFTER_WHEEL_UNLOK_MS 1000 // после того как колесо разблокировалось
-#define ALL_OFF_IF_WHEEL_DONT_UNLOK_MS 2000 // если колесо не разблокировалось
+#define ALL_OFF_IF_WHEEL_DONT_UNLOK_MS 1000 // если колесо не разблокировалось
 
 bool absActive = false; //если true то abs активна (скорость 10км/ч и больше)
 bool wheelLocked = false; // если true то колесо заблокировано
@@ -30,21 +29,14 @@ static void IRAM_ATTR handleInterrupt(void)
 
 void solenoidValveClose()
 {
-  digitalWrite(PIN_SOLENOID_1, HIGH);
-  digitalWrite(PIN_SOLENOID_2, LOW);
+  digitalWrite(PIN_VALVE, HIGH);
 }
 
 void solenoidValveOpen()
 {
-  digitalWrite(PIN_SOLENOID_1, LOW);
-  digitalWrite(PIN_SOLENOID_2, HIGH);
+  digitalWrite(PIN_VALVE, LOW);
 }
 
-void solenoidValveOff()
-{
-  digitalWrite(PIN_SOLENOID_1, LOW);
-  digitalWrite(PIN_SOLENOID_2, LOW);
-}
 
 void pumpOn()
 {
@@ -59,7 +51,7 @@ void pumpOff()
 void allOff()
 {
   // TODO вероятно,что здесь надо ставить wheelLocked = false;
-  solenoidValveOff();
+  solenoidValveOpen();
   pumpOff();
 }
 
@@ -75,8 +67,8 @@ void checkImpulseFunc()
   else if(interruptCounter && wheelLocked)
   {
     wheelLocked = false;
-    solenoidValveOpen();
-    tickerAllOff.once_ms(ALL_OFF_AFTER_WHEEL_UNLOK_MS , allOff);
+    tickerAllOff.detach();
+    allOff();
   }
   absActive = interruptCounter >= MAX_IMPULSES_PER_CHECK_INTERVAL;
   interruptCounter = 0;
@@ -84,8 +76,7 @@ void checkImpulseFunc()
 
 void setup() {
 
-  pinMode(PIN_SOLENOID_1, OUTPUT);
-  pinMode(PIN_SOLENOID_2, OUTPUT);
+  pinMode(PIN_VALVE, OUTPUT);
   pinMode(PIN_PUMP, OUTPUT);
   allOff();
   attachInterrupt(digitalPinToInterrupt(PIN_HALL_EFFECT_SENSOR), handleInterrupt, FALLING);
